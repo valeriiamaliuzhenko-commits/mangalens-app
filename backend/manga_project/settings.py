0e8e3bc6b5116e0486a1013afec11d9df9681fc1
Lera <valeriia.maliuzhenko@nure.ua>
2026-06-17 13:49:51 +0300

    storage issue fix

--- backend/manga_project/settings.py
+++ backend/manga_project/settings.py
@@ -1,7 +1,4 @@
-"""
-Django settings for manga_project.
-"""
-
+import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -49,12 +46,19 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
 ]
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
